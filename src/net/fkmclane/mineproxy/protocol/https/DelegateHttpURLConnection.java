8cd861fd93bea0ce9bcd4071037c7f336c584271
Foster McLane <fkmclane@gmail.com>
2014-07-20 14:45:42 -0400

    Add fake https protocol implementation that uses http

---
+++ src/net/fkmclane/mineproxy/protocol/https/DelegateHttpURLConnection.java
@@ -0,0 +1,33 @@
+package net.fkmclane.mineproxy.protocol.https;
+
+import java.net.URL;
+import java.net.Proxy;
+import java.io.*;
+
+import sun.net.www.protocol.http.HttpURLConnection;
+
+public class DelegateHttpURLConnection extends HttpURLConnection {
+	protected DelegateHttpURLConnection(URL url, Proxy proxy, Handler handler) {
+		super(url, proxy,handler);
+	}
+
+	protected void setNewClient(URL url) throws IOException {
+		super.setNewClient(url, false);
+	}
+
+	protected void setNewClient(URL url, boolean useCache) throws IOException {
+		super.setNewClient(url, useCache);
+	}
+
+	protected void setProxiedClient(URL url, String proxyHost, int proxyPort) throws IOException {
+		super.setProxiedClient(url, proxyHost, proxyPort);
+	}
+
+	protected void setProxiedClient(URL url, String proxyHost, int proxyPort, boolean useCache) throws IOException {
+		super.setProxiedClient(url, proxyHost, proxyPort, useCache);
+	}
+
+	protected void finalize() throws Throwable {
+		super.finalize();
+	}
+}
