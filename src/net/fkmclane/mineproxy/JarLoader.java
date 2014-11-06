6ad44cb99fcfeb789191bd70b148fd9a4c75e32d
Foster McLane <fkmclane@gmail.com>
2014-11-06 13:09:45 -0500

    Update JarLoader to add jar to classpath

--- src/net/fkmclane/mineproxy/JarLoader.java
+++ src/net/fkmclane/mineproxy/JarLoader.java
@@ -10,19 +10,25 @@ import java.net.URLClassLoader;
 import java.util.jar.Attributes;
 
 public class JarLoader {
-	public static void run(File jar, String[] args) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, IOException, NoSuchMethodException {
+	public static void run(File jar, String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
 		run(jar.toURI().toURL(), args);
 	}
 
-	public static void run(URL jar, String[] args) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, IOException, NoSuchMethodException {
-		URLClassLoader loader = new URLClassLoader(new URL[] { jar });
-		JarURLConnection conn = (JarURLConnection)new URL("jar", "", jar + "!/").openConnection();
-		String mainclassname = conn.getMainAttributes().getValue(Attributes.Name.MAIN_CLASS);
+	public static void run(URL jar, String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		ClassLoader loader = ClassLoader.getSystemClassLoader();
 
-		Class<?> mainclass = loader.loadClass(mainclassname);
-		Method main = mainclass.getMethod("main", new Class[] { args.getClass() });
+		//Add jar to classpath
+		Method add_url = URLClassLoader.class.getDeclaredMethod("addURL", new Class[] { URL.class });
+		add_url.setAccessible(true);
+		add_url.invoke(loader, new Object[] { jar });
+
+		//Get main class
+		JarURLConnection jarconn = (JarURLConnection)new URL("jar", "", jar + "!/").openConnection();
+		Class<?> mainclass = loader.loadClass(jarconn.getMainAttributes().getValue(Attributes.Name.MAIN_CLASS));
+
+		//Invoke main method in main class
+		Method main = mainclass.getDeclaredMethod("main", new Class[] { String[].class });
 		main.setAccessible(true);
-
 		main.invoke(null, new Object[] { args });
 	}
 }
